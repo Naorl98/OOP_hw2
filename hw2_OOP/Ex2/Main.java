9b5b2eadf7147ad914cae3ad0c8d5b207bdde145
Naorl98 <118805710+Naorl98@users.noreply.github.com>
2023-01-09 16:18:45 +0200

    Delete hw2_OOP directory

--- hw2_OOP/Ex2/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        int count =  Ex2_1.getNumOfLines( Ex2_1.createTextFiles( 10000, 2, 10000));
-        System.out.println("lines = " + count);
-
-    }
-}
